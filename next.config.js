4002aad74c067ab60791e6f6d89910c0cc53bd1d
bangas <sahil.banga@ril.com>
2022-11-09 18:09:55 +0530

    initial

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  webpack5: true, // Need to make it true for some versions of Next JS
+  distDir: 'build', // Defined build directory
+  webpack: (config, options) => { // webpack configurations
+    config.plugins.push(
+        new options.webpack.container.ModuleFederationPlugin({
+          name:"fe1",
+          filename: "remoteEntry.js", // remote file name which will used later
+          remoteType: "script",
+          exposes: { // expose all component here.
+            "./header": "./src/component/Header"
+          },
+          shared: [
+            {
+              react: {
+                eager: true,
+                singleton: true,
+                requiredVersion: false,
+              }
+            },
+            {
+              "react-dom": {
+                eager: true,
+                singleton: true,
+                requiredVersion: false,
+              }
+            },
+          ]
+        })
+    )
+    return config
+  }
 }
 
 module.exports = nextConfig
